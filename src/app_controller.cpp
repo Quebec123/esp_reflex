192dbcc88febf96ad4501d2737578ab1571030ee
wiktor.sobczynski_278360 <278360@student.pwr.edu.pl>
2024-11-27 12:07:47 +0100

    comments

--- src/app_controller.cpp
+++ src/app_controller.cpp
@@ -192,46 +192,81 @@ static void execute_led_pattern(const LedPattern<StageCount>& pattern,
 
 }    // namespace impl
 
+/**
+ * @brief Main control loop for the application.
+ *
+ * This function initializes the GPIO, random number generator, and I2C devices.
+ * It then enters an infinite loop where it executes LED patterns and waits for
+ * user input to start the game. The function never returns.
+ */
 [[noreturn]] void take_control_no_return() noexcept {
+  // Initialize GPIO pins
   impl::init_gpio();
+  // Initialize random number generator
   impl::init_random();
+  // Initialize I2C devices
   impl::init_i2c_devices();
 
+  // Atomic flag to control the stopping of LED patterns
   std::atomic_bool stop_token = false;
 
+  // Log the execution of the check pattern
   ESP_LOGE("TEST", "EXECUTING CHECK PATTERN");
+  // Execute the check LED pattern
   impl::execute_led_pattern(led_pattern::check, stop_token);
 
+  // Main control loop
   while (true) {
+    // Create a thread to execute the general LED pattern
     std::thread general_pattern_thread {[&stop_token]() noexcept {
       while (true) {
+        // Log the execution of the general pattern
         ESP_LOGE("TEST", "EXECUTING GENERAL PATTERN");
+        // Execute the general LED pattern
         impl::execute_led_pattern(led_pattern::general, stop_token);
 
+        // If the stop token is set, exit the thread
         if (stop_token.load()) {
           return;
         }
       }
     }};
 
+    // Wait for the start button to be pressed
     app::game::wait_for_start_press();
 
+    // Set the stop token to stop the general pattern thread
     stop_token.store(true);
+    // Wait for the general pattern thread to finish
     general_pattern_thread.join();
+    // Reset the stop token
     stop_token.store(false);
 
+    // Log the execution of the start pattern
     ESP_LOGE("TEST", "EXECUTING START PATTERN");
+    // Execute the start LED pattern
     impl::execute_led_pattern(led_pattern::start, stop_token);
 
+    // Start the game
     app::game::play();
 
+    // Log the execution of the end pattern
     ESP_LOGE("TEST", "EXECUTING END PATTERN");
+    // Execute the end LED pattern
     impl::execute_led_pattern(led_pattern::end, stop_token);
   }
 }
 
 namespace gpio {
 
+/**
+ * @brief Turns on the specified pin for the given output type.
+ *
+ * This function sets the specified pin to a high level (on) for the given output type.
+ *
+ * @param pin The pin number to turn on.
+ * @param output The type of output to control (Players, SegPlayer1, SegPlayer2, SegTimer, Gpio).
+ */
 void turn_on(const uint8_t pin, Output output) noexcept {
   switch (output) {
     case Output::Players:
@@ -253,6 +288,14 @@ void turn_on(const uint8_t pin, Output output) noexcept {
   }
 }
 
+/**
+ * @brief Turns off the specified pin for the given output type.
+ *
+ * This function sets the specified pin to a low level (off) for the given output type.
+ *
+ * @param pin The pin number to turn off.
+ * @param output The type of output to control (Players, SegPlayer1, SegPlayer2, SegTimer, Gpio).
+ */
 void turn_off(const uint8_t pin, Output output) noexcept {
   switch (output) {
     case Output::Players:
@@ -273,6 +316,14 @@ void turn_off(const uint8_t pin, Output output) noexcept {
   }
 }
 
+/**
+ * @brief Turns on the specified row of LEDs for the given player.
+ *
+ * This function sets the specified row of LEDs to a high level (on) for the given player.
+ *
+ * @param player The player whose row of LEDs to turn on (Player1 or Player2).
+ * @param row The row of LEDs to turn on (Bottom, MiddleBottom, MiddleTop, Top).
+ */
 void turn_on_row(Player player, Row row) noexcept {
   switch (player) {
     case Player::Player1:
@@ -389,12 +440,21 @@ void all_off() noexcept {
   turn_off(config::gpio::start_out, Output::Gpio);
 }
 
+/**
+ * @brief Displays a two-digit number on a 7-segment display.
+ *
+ * This function takes a number between 0 and 99 and displays it on a specified
+ * 7-segment display. If the number is greater than 99, it will display 99.
+ *
+ * @param number The number to display (0-99).
+ * @param display The 7-segment display to use (Player1, Player2, Timer).
+ */
 void display_segment_number(uint8_t number, SegmentDisplay display) noexcept {
   if (number > 99) {
     ESP_LOGE("LedPattern", "Number out of range, displaying max number");
     number = 99;
   }
-
+//
   const uint8_t tens = number / 10;
   const uint8_t ones = number % 10;
 
@@ -434,6 +494,13 @@ void display_segment_number(uint8_t number, SegmentDisplay display) noexcept {
   }
 }
 
+/**
+ * @brief Turns off all segments of a specified 7-segment display.
+ *
+ * This function sets all pins of the specified 7-segment display to a low level (off).
+ *
+ * @param display The 7-segment display to turn off (Player1, Player2, Timer).
+ */
 void turn_off_segment(SegmentDisplay display) noexcept {
   for (const uint8_t& pin : config::mcp::seg_left_pins) {
     switch (display) {
@@ -467,10 +534,21 @@ void turn_off_segment(SegmentDisplay display) noexcept {
 
 namespace util {
 
+/**
+ * @brief Gets random player pins for the specified player.
+ *
+ * This function returns a pair of input and output pins for the specified player.
+ * The pins are selected randomly from the player's available pins.
+ *
+ * @param player The player for whom to get the random pins (Player1 or Player2).
+ * @return A pair of input and output pins for the specified player.
+ */
 [[nodiscard]] gpio::PlayerPins get_random_player_pins(Player player) noexcept {
+  // Generate a random index within the range of available pins
   const uint8_t random_index =
   static_cast<const uint8_t>(random(config::gpio::player1_in.size()));
 
+  // Return the corresponding input and output pins for the specified player
   if (player == Player::Player1) {
     return {config::gpio::player1_in.at(random_index),
             config::mcp::player1_out.at(random_index)};
@@ -480,26 +558,43 @@ namespace util {
   }
 }
 
+/**
+ * @brief Waits for a specified delay or until a stop token is set.
+ *
+ * This function waits for the given delay in milliseconds, checking periodically
+ * if the stop token is set. If the stop token is set during the wait, the function
+ * returns true immediately. Otherwise, it returns false after the delay.
+ *
+ * @param delayMs The delay in milliseconds to wait.
+ * @param stop_token The atomic boolean flag to check for stopping the wait.
+ * @return true if the stop token is set during the wait, false otherwise.
+ */
 [[nodiscard]] bool wait_stop_token(
 const uint32_t&         delayMs,
 const std::atomic_bool& stop_token) noexcept {
+  // If the stop token is already set, return true immediately
   if (stop_token.load()) {
     return true;
   }
 
+  // If the delay is zero, return false immediately
   if (delayMs == 0) {
     return false;
   }
 
+  // Wait for the specified delay, checking the stop token periodically
   for (uint32_t wait_time  = 0; wait_time < delayMs;
        wait_time          += config::min_delay) {
+    // If the stop token is set, return true
     if (stop_token.load()) {
       return true;
     }
 
+    // Delay for the minimum delay period
     vTaskDelay(config::min_delay / portTICK_PERIOD_MS);
   }
 
+  // Return false if the delay completes without the stop token being set
   return false;
 }
 
--- src/app_game.cpp
+++ src/app_game.cpp
@@ -39,28 +39,59 @@ using QueueStorage = std::array<uint8_t,
   return s_queue_handle;
 }
 
+/**
+ * @brief ISR handler for button GPIO interrupts.
+ *
+ * This function is called when a button GPIO interrupt occurs. It sends the GPIO number
+ * to the queue and yields from the ISR if a higher priority task was woken.
+ *
+ * @param gpio_arg Pointer to the GPIO number that triggered the interrupt.
+ */
 static void IRAM_ATTR isr_buttons_gpio(void* gpio_arg) noexcept {
+  // Convert the GPIO argument to a uint8_t GPIO number
   const auto gpio_num =
   static_cast<uint8_t>(reinterpret_cast<ptrdiff_t>(gpio_arg));
 
   BaseType_t higher_priority_task_woken = pdFALSE;
+  // Send the GPIO number to the queue from the ISR
   xQueueSendFromISR(get_gpio_queue(), &gpio_num, &higher_priority_task_woken);
 
+  // Yield from the ISR if a higher priority task was woken
   if (higher_priority_task_woken == pdTRUE) {
     portYIELD_FROM_ISR();
   }
 }
-
+/**
+ * @brief Retrieves the final score of the game.
+ *
+ * This function returns a reference to a static `FinalScore` object that holds
+ * the final scores for both players. The initial scores are set to the maximum
+ * score defined in the configuration.
+ *
+ * @return A reference to the `FinalScore` object containing the final scores.
+ */
 [[nodiscard]] static FinalScore& get_final_score() noexcept {
+  // Static variable to hold the final scores, initialized to max scores
   static FinalScore s_final_score = {config::game::max_score,
                                      config::game::max_score};
   return s_final_score;
 }
 
+/**
+ * @brief Generates a random player pin different from the current one.
+ *
+ * This function generates a random pin index for a player that is different
+ * from the provided current pin index. It ensures that the new pin index
+ * is not the same as the current one.
+ *
+ * @param current The current pin index that should not be selected.
+ * @return A new random pin index different from the current one.
+ */
 [[nodiscard]] static uint8_t generate_random_player_pin(
 uint8_t current) noexcept {
   uint8_t pin = current;
 
+  // Generate a new random pin index until it is different from the current one
   while (pin == current) {
     pin = random() % config::gpio::player1_in.size();
   }
@@ -68,16 +99,36 @@ uint8_t current) noexcept {
   return pin;
 }
 
+/**
+ * @brief Attaches the ISR handler for the start button GPIO interrupt.
+ *
+ * This function adds the ISR handler for the start button GPIO interrupt.
+ * It associates the `isr_buttons_gpio` function with the GPIO number
+ * specified in the configuration for the start button.
+ */
 static void attach_isr_start() noexcept {
   gpio_isr_handler_add(static_cast<gpio_num_t>(config::gpio::start_in),
                        isr_buttons_gpio,
                        reinterpret_cast<void*>(config::gpio::start_in));
 }
 
+/**
+ * @brief Detaches the ISR handler for the start button GPIO interrupt.
+ *
+ * This function removes the ISR handler associated with the start button GPIO
+ * interrupt, effectively disabling the interrupt for the start button.
+ */
 static void detach_isr_start() noexcept {
   gpio_isr_handler_remove(static_cast<gpio_num_t>(config::gpio::start_in));
 }
 
+/**
+ * @brief Attaches the ISR handlers for the player buttons GPIO interrupts.
+ *
+ * This function adds the ISR handlers for the player buttons GPIO interrupts.
+ * It associates the `isr_buttons_gpio` function with the GPIO numbers
+ * specified in the configuration for both player 1 and player 2 buttons.
+ */
 static void attach_isr_players() noexcept {
   for (const uint8_t& pin : config::gpio::player1_in) {
     gpio_isr_handler_add(static_cast<gpio_num_t>(pin),
@@ -92,6 +143,12 @@ static void attach_isr_players() noexcept {
   }
 }
 
+/**
+ * @brief Detaches the ISR handlers for the player buttons GPIO interrupts.
+ *
+ * This function removes the ISR handlers associated with the player buttons GPIO
+ * interrupts, effectively disabling the interrupts for both player 1 and player 2 buttons.
+ */
 static void detach_isr_players() noexcept {
   for (const uint8_t& pin : config::gpio::player1_in) {
     gpio_isr_handler_remove(static_cast<gpio_num_t>(pin));
@@ -104,6 +161,14 @@ static void detach_isr_players() noexcept {
 
 }    // namespace impl
 
+/**
+ * @brief Waits for the start button press.
+ *
+ * This function waits for the start button to be pressed by attaching the ISR handler
+ * for the start button GPIO interrupt. It continuously checks the GPIO queue until
+ * the start button GPIO number is received, indicating the button press. Once the
+ * button is pressed, it detaches the ISR handler.
+ */
 void wait_for_start_press() noexcept {
   impl::attach_isr_start();
 
@@ -115,26 +180,39 @@ void wait_for_start_press() noexcept {
   impl::detach_isr_start();
 }
 
+/**
+ * @brief Executes the main game loop.
+ *
+ * This function runs the main game loop, handling the game logic for both players.
+ * It initializes the ISR handlers for the player buttons, sets up the game timer,
+ * and manages the game state until one of the players reaches the maximum score
+ * or the timer runs out. The function also updates the display with the current
+ * scores and the remaining time.
+ */
 void play() noexcept {
   ESP_LOGE("TEST", "GAME_BEGIN");
 
+  // Attach ISR handlers for player buttons
   impl::attach_isr_players();
 
+  // Generate initial random target pins for both players
   uint8_t player1_target_index =
   impl::generate_random_player_pin(std::numeric_limits<uint8_t>::max());
   uint8_t player2_target_index =
   impl::generate_random_player_pin(std::numeric_limits<uint8_t>::max());
 
+  // Initialize player scores
   uint8_t player1_score = 0;
   uint8_t player2_score = 0;
 
+  // Initialize game timer and stop token
   std::atomic_int8_t timer            = config::game::game_time;
   std::atomic_bool   timer_stop_token = false;
 
+  // Start the game timer thread
   std::thread timer_thread([&timer, &timer_stop_token]() {
     controller::gpio::display_segment_number(static_cast<uint8_t>(timer),
                                              SegmentDisplay::Timer);
-    //ESP_LOGE("TIMER","%u", static_cast<uint8_t>(timer));
     while (timer > 0) {
       if (controller::util::wait_stop_token(1000, timer_stop_token)) {
         break;
@@ -143,21 +221,22 @@ void play() noexcept {
       --timer;
       controller::gpio::display_segment_number(static_cast<uint8_t>(timer),
                                                SegmentDisplay::Timer);
-
     }
   });
 
+  // Display initial scores
   controller::gpio::display_segment_number(player1_score,
                                            SegmentDisplay::Player1);
   controller::gpio::display_segment_number(player2_score,
                                            SegmentDisplay::Player2);
 
+  // Turn on initial target pins for both players
   controller::gpio::turn_on(config::mcp::player1_out.at(player1_target_index),
                             Output::Players);
-  //ESP_LOGE("TEST","%u pin for player 1",player1_target_index);
   controller::gpio::turn_on(config::mcp::player2_out.at(player2_target_index),
                             Output::Players);
-  //ESP_LOGE("TEST","%u pin for player 2",player2_target_index);
+
+  // Main game loop
   while (player1_score < config::game::max_score &&
          player2_score < config::game::max_score && timer > 0) {
     uint8_t gpio_num = std::numeric_limits<uint8_t>::max();
@@ -169,6 +248,7 @@ void play() noexcept {
       break;
     }
 
+    // Check if player 1 pressed the correct button
     if (gpio_num == config::gpio::player1_in.at(player1_target_index)) {
       controller::gpio::turn_off(
       config::mcp::player1_out.at(player1_target_index),
@@ -182,7 +262,6 @@ void play() noexcept {
 
       controller::gpio::display_segment_number(player1_score,
                                                SegmentDisplay::Player1);
-     ESP_LOGE("TEST", "%u score", player1_score);
       player1_target_index =
       impl::generate_random_player_pin(player1_target_index);
 
@@ -193,7 +272,9 @@ void play() noexcept {
       controller::gpio::turn_on(
       config::mcp::player1_out.at(player1_target_index),
       Output::Players);
-    } else if (gpio_num == config::gpio::player2_in.at(player2_target_index)) {
+    }
+    // Check if player 2 pressed the correct button
+    else if (gpio_num == config::gpio::player2_in.at(player2_target_index)) {
       controller::gpio::turn_off(
       config::mcp::player2_out.at(player2_target_index),
       Output::Players);
@@ -206,7 +287,6 @@ void play() noexcept {
 
       controller::gpio::display_segment_number(player2_score,
                                                SegmentDisplay::Player2);
-     // ESP_LOGE("TEST","%u score", player2_score);
       player2_target_index =
       impl::generate_random_player_pin(player2_target_index);
 
@@ -220,11 +300,14 @@ void play() noexcept {
     }
   }
 
+  // Detach ISR handlers for player buttons
   impl::detach_isr_players();
 
+  // Stop the timer thread
   timer_stop_token = true;
   timer_thread.join();
 
+  // Store the final scores
   impl::get_final_score() = {player1_score, player2_score};
 }
 
